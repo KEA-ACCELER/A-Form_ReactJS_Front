73e74a1d469ba37f5e4128e09d9a4cc635e1b6e6
masibasi <jimin_lee98@naver.com>
2023-03-20 00:33:55 +0900

    chore : replace buttons, update btn css

--- src/components/CreateSurvey.js
+++ src/components/CreateSurvey.js
@@ -3,70 +3,77 @@ import { Form } from "react-bootstrap";
 import QuestionForm from "./forms/QuestionForm";
 import AddingOption from "./forms/AddingOption";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
 function CreateSurvey() {
-  const [questions, setQuestions] = useState([]); //index, state(어떤 타입의 질문인지)
+    const [questions, setQuestions] = useState([]); //index, state(어떤 타입의 질문인지)
 
-  // TODO : X 표시를 누르면 해당 문제의 정보가 삭제된다.
-  function delQuestion(index) {
-    questions.splice(index, 1);
-    setQuestions([...questions]);
-  }
-
-  function addQuestion(input) {
-    if (questions == null) {
-      questions.push({
-        questionType: input,
-        questionTitle: "",
-        item: [],
-      });
-    } else {
-      questions.push({
-        questionType: input,
-        questionTitle: "",
-        item: [],
-      });
+    // TODO : X 표시를 누르면 해당 문제의 정보가 삭제된다.
+    function delQuestion(index) {
+        questions.splice(index, 1);
+        setQuestions([...questions]);
     }
-    setQuestions([...questions]);
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(questions);
-  }
+    function addQuestion(input) {
+        if (questions == null) {
+            questions.push({
+                questionType: input,
+                questionTitle: "",
+                item: [],
+            });
+        } else {
+            questions.push({
+                questionType: input,
+                questionTitle: "",
+                item: [],
+            });
+        }
+        setQuestions([...questions]);
+    }
 
-  return (
-    <div className="createSurvey">
-      <Container>
-        <Row>
-          <Col>
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(questions);
+    }
+
+    return (
+        <Container className="CreateSurvey">
             <h1>Create Survey</h1>
-            <AddingOption addQuestion={addQuestion}></AddingOption>
-            <Form onSubmit={handleSubmit}>
-              {questions.map((q, index) => {
-                return (
-                  <QuestionForm
-                    questionType={q.questionType}
-                    delQuestion={delQuestion}
-                    q={q}
-                    qIndex={index}
-                    questions={questions}
-                    setQuestions={setQuestions}
-                  />
-                );
-              })}
-              <Button type="submit" variant="danger">
-                Create Survey
-              </Button>
+            <div className="ButtonWrapper">
+                <AddingOption addQuestion={addQuestion}></AddingOption>
+                <div className="SurveyBtnWrapper">
+                    <Button
+                        className="submit-btn"
+                        type="submit"
+                        variant="outline-success"
+                    >
+                        Create Survey
+                    </Button>
+                    <Button
+                        className="delete-btn"
+                        type="submit"
+                        variant="outline-danger"
+                    >
+                        Delete Survey
+                    </Button>
+                </div>
+            </div>
+            <Form className="Form" onSubmit={handleSubmit}>
+                {questions.map((q, index) => {
+                    return (
+                        <QuestionForm
+                            questionType={q.questionType}
+                            delQuestion={delQuestion}
+                            q={q}
+                            qIndex={index}
+                            questions={questions}
+                            setQuestions={setQuestions}
+                        />
+                    );
+                })}
             </Form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+        </Container>
+    );
 }
 
 export default CreateSurvey;
--- src/components/forms/QuestionForm.js
+++ src/components/forms/QuestionForm.js
@@ -7,49 +7,49 @@ import ShortForm from "./ShortForm";
 import Checkbox from "./Checkbox";
 
 function QuestionForm(props) {
-  const questionType = props.questionType;
+    const questionType = props.questionType;
 
-  return (
-    <Card style={{ maxWidth: "1024px" }}>
-      <Card.Body>
-        <Card.Title>{props.q.questionTitle}</Card.Title>
-        <Button
-          variant="outline-danger"
-          onClick={(e) => {
-            props.delQuestion(props.qIndex);
-          }}
-        >
-          X
-        </Button>
-        <div className="questionType">
-          {questionType === 1 && (
-            <RadioButton
-              q={props.q}
-              qIndex={props.qIndex}
-              questions={props.questions}
-              setQuestions={props.setQuestions}
-            />
-          )}
-          {questionType === 2 && (
-            <Checkbox
-              q={props.q}
-              qIndex={props.qIndex}
-              questions={props.questions}
-              setQuestions={props.setQuestions}
-            />
-          )}
-          {questionType === 3 && (
-            <ShortForm
-              q={props.q}
-              qIndex={props.qIndex}
-              questions={props.questions}
-              setQuestions={props.setQuestions}
-            />
-          )}
-        </div>
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card className="QuestionForm" style={{ maxWidth: "1024px" }}>
+            <Card.Body>
+                <Card.Title>{props.q.questionTitle}</Card.Title>
+                <Button
+                    variant="outline-danger"
+                    onClick={(e) => {
+                        props.delQuestion(props.qIndex);
+                    }}
+                >
+                    X
+                </Button>
+                <div className="questionType">
+                    {questionType === 1 && (
+                        <RadioButton
+                            q={props.q}
+                            qIndex={props.qIndex}
+                            questions={props.questions}
+                            setQuestions={props.setQuestions}
+                        />
+                    )}
+                    {questionType === 2 && (
+                        <Checkbox
+                            q={props.q}
+                            qIndex={props.qIndex}
+                            questions={props.questions}
+                            setQuestions={props.setQuestions}
+                        />
+                    )}
+                    {questionType === 3 && (
+                        <ShortForm
+                            q={props.q}
+                            qIndex={props.qIndex}
+                            questions={props.questions}
+                            setQuestions={props.setQuestions}
+                        />
+                    )}
+                </div>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default QuestionForm;
