bd6aedf13c2b6c8429580c2b7276b9aed2d2119f
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-04-08 16:19:06 +0900

    temp

--- src/components/FormList.js
+++ src/components/FormList.js
@@ -4,15 +4,15 @@ import Alert from "react-bootstrap/Alert";
 import { FormStateContext } from "../App";
 
 export const FormList = () => {
-    const navigation = useNavigate();
-    const formData = useContext(FormStateContext);
-    return (
-        <div className="formList">
-            {formData.map((it) => (
-                <div key={it.id} className="formListElem">
-                    Title : {it.formTitle}
-                </div>
-            ))}
+  const navigation = useNavigate();
+  const formData = useContext(FormStateContext);
+  return (
+    <div className="formList">
+      {formData.map((it) => (
+        <div key={it.id} className="formListElem">
+          Title : {it.formTitle}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
