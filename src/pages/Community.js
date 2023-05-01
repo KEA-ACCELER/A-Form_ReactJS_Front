9cb86293ef2f300e7c28e92e4a3f48a3d8a7cda3
masibasi <jimin_lee98@naver.com>
2023-05-01 22:50:13 +0900

    community componentization

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,6 +1,6 @@
-import { Button } from "react-bootstrap";
 import "./Community.css";
-import AB from "../assets/images/AB.png";
+import { HotAvsBSurvey, HotCategory, HotSurvey } from "../components/HotSurvey";
+
 export const Community = () => {
     return (
         <div className="Community">
@@ -12,18 +12,9 @@ export const Community = () => {
                 <a>더보기</a>
             </div>
             <div className="AvsBWrapper">
-                <div className="formWrapper">
-                    <img src={AB} />
-                    <p className="AvsBTitle">설문1</p>
-                </div>
-                <div className="formWrapper">
-                    <img src={AB} />
-                    <p className="AvsBTitle">설문1</p>
-                </div>
-                <div className="formWrapper">
-                    <img src={AB} />
-                    <p className="AvsBTitle">설문1</p>
-                </div>
+                <HotAvsBSurvey />
+                <HotAvsBSurvey />
+                <HotAvsBSurvey />
             </div>
             <div className="titleWrapper">
                 <h4 className="title">많이 본 설문</h4>
@@ -34,36 +25,22 @@ export const Community = () => {
                         <h5>Hot 설문</h5>
                         <a>더보기</a>
                     </div>
-                    <div className="hotSurvey">
-                        <p className="surveyTitle">어쩌구 저쩌구 1</p>
-                        <p className="date">2023. 03. 01</p>
-                        <p className="status">진행중</p>
-                    </div>
-                    <div className="hotSurvey">
-                        <p className="surveyTitle">어쩌구 저쩌구 1</p>
-                        <p className="date">2023. 03. 01</p>
-                        <p className="status">진행중</p>
-                    </div>
-                    <div className="hotSurvey">
-                        <p className="surveyTitle">어쩌구 저쩌구 1</p>
-                        <p className="date">2023. 03. 01</p>
-                        <p className="status">진행중</p>
-                    </div>
+                    <HotSurvey />
+                    <HotSurvey />
+                    <HotSurvey />
+                    <HotSurvey />
+                    <HotSurvey />
                 </div>
                 <div className="hotCategoryWrapper">
                     <div className="title2Wrapper">
                         <h5>Hot 분야</h5>
                         <a>더보기</a>
                     </div>
-                    <div className="hotSurvey">
-                        <p className="surveyTitle">어쩌구 저쩌구 1</p>
-                    </div>
-                    <div className="hotSurvey">
-                        <p className="surveyTitle">어쩌구 저쩌구 1</p>
-                    </div>
-                    <div className="hotSurvey">
-                        <p className="surveyTitle">어쩌구 저쩌구 1</p>
-                    </div>
+                    <HotCategory />
+                    <HotCategory />
+                    <HotCategory />
+                    <HotCategory />
+                    <HotCategory />
                 </div>
             </div>
         </div>
