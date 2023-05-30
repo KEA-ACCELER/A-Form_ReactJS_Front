0b90aa06e9a4146fffe8a99fb1f8b3d18a6c509a
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-05-30 11:50:33 +0900

    temp

--- src/components/SurveyList/SurveyListItem.js
+++ src/components/SurveyList/SurveyListItem.js
@@ -3,48 +3,49 @@ import AB from "../../assets/images/AB.png";
 import { useNavigate } from "react-router-dom";
 import "./SurveyListItem.css";
 export const SurveyListItem = (props) => {
-    const navigate = useNavigate();
-    return (
-        <div
-            className="SurveyListItem"
-            onClick={() => {
-                navigate(`/create`, { state: { id: props.id } });
-            }}
-        >
-            <span className="surveyTitle">{props.title}</span>
-            <span className="author">작성자 : {props.author}</span>
-            <span className="date">{props.date}</span>
-            <span className={`status ${props.status}`}>{props.status}</span>
-        </div>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="SurveyListItem"
+      onClick={() => {
+        navigate(`/create`, { state: { id: props.id } });
+      }}
+    >
+      <span className="surveyTitle">{props.title}</span>
+      <span className="author">작성자 : {props.author}</span>
+      <span className="date">{props.date}</span>
+      <span className={`status ${props.status}`}>{props.status}</span>
+    </div>
+  );
 };
 export const HotCategory = (props) => {
-    return (
-        <div className="SurveyListItem">
-            <span className="surveyTitle">{props.category}</span>
-        </div>
-    );
+  return (
+    <div className="SurveyListItem">
+      <span className="surveyTitle">{props.category}</span>
+    </div>
+  );
 };
 
 export const HotAvsBSurvey = (props) => {
-    return (
-        <div className="HotAvsBSurvey  hvr-glow">
-            <img src={props.img} alt="" />
-            <span className="AvsBTitle">{props.title}</span>
-        </div>
-    );
+  return (
+    <div className="HotAvsBSurvey  hvr-glow">
+      <img src={props.img} alt="" />
+      <span className="AvsBTitle">{props.title}</span>
+    </div>
+  );
 };
 
 SurveyListItem.defaultProps = {
-    title: "제목",
-    date: "2023. 04. 15",
-    status: "OPEN",
-    author: "작성자",
+  title: "제목",
+  date: "2023. 04. 15",
+  status: "OPEN",
+  author: "작성자",
 };
 HotCategory.defaultProps = {
-    category: "분야 예시 1",
+  category: "분야 예시 1",
 };
 HotAvsBSurvey.defaultProps = {
-    img: AB,
-    title: "설문1",
+  img: AB,
+  title: "설문1",
 };
