bc00c3f2509a9bffed19c6622eb4e52e31f629f2
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-06-03 21:39:52 +0900

    Show statistics in pieChart

--- src/pages/Post/Statistics/StatList.js
+++ src/pages/Post/Statistics/StatList.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
 import StatListItem from "./StatListItem";
 
-const StatList = ({ statistics }) => {
+const StatList = ({ statistics, postId, surveyData }) => {
   useEffect(() => {
     console.log("statstat : ", statistics);
   }, [statistics]);
   return (
     <div className="StatList">
-      {statistics === null ? null : statistics.statistics.map((item, index) => <StatListItem key={item.index} item={item} questionNumber={index + 1} />)}
+      {statistics === null || surveyData === null || !surveyData.questions || surveyData.questions.length !== statistics.statistics.length
+        ? null
+        : statistics.statistics.map((item, index) => (
+            <StatListItem
+              key={item.index}
+              item={item}
+              questionNumber={index + 1}
+              postId={postId}
+              questionTitle={surveyData.questions[index].title}
+              selections={surveyData.questions[index].selections}
+            />
+          ))}
     </div>
   );
 };
