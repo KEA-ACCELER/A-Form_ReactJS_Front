c500a33d3e37b533880ccb022ff1ec7735d87b88
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-06-06 23:18:06 +0900

    Can see shortform answer in statistics

--- src/components/SurveyList/SurveyList.js
+++ src/components/SurveyList/SurveyList.js
@@ -64,39 +64,10 @@ export const SurveyList = ({ type, page, offset, status, sort, date }) => {
       setFormData(modifiedData);
       // setFormData(result);
     } else if (type === "popular") {
-      // 인기 설문
-      // const now = new Date(); // 현재 시간 받기
-      // console.log("now: ", now);
-
-      // const year = now.getFullYear();
-      // const month = String(now.getMonth() + 1).padStart(2, "0");
-      // const date = String(now.getDate()).padStart(2, "0");
-      // const hours = String(now.getHours()).padStart(2, "0");
-      // const minutes = String(now.getMinutes()).padStart(2, "0");
-      // const seconds = String(now.getSeconds()).padStart(2, "0");
-      // const offsetMinutes = now.getTimezoneOffset(); // 현재 시간의 UTC와 지역 시간 간격 (분 단위)
-      // const offsetHours = -Math.floor(offsetMinutes / 60); // UTC와 지역 시간 간격 (시간 단위)
-      // const offsetSign = offsetHours >= 0 ? "+" : "-";
-      // const offsetFormatted = `${offsetSign}${Math.abs(offsetHours).toString().padStart(2, "0")}:00`; // 시간 형식 맞추기 ("+09:00")
-      // const formattedDateTime = `${year}-${month}-${date}T${hours}:${minutes}:${seconds}${offsetFormatted}`;
-      // console.log("date: ", formattedDateTime);
-
-      const now = new Date(); // 현재 로컬 시간 받기
-
-      const utcNow = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds()));
-
-      const year = utcNow.getUTCFullYear();
-      const month = String(utcNow.getUTCMonth() + 1).padStart(2, "0");
-      const date = String(utcNow.getUTCDate()).padStart(2, "0");
-      const hours = String(utcNow.getUTCHours()).padStart(2, "0");
-      const minutes = String(utcNow.getUTCMinutes()).padStart(2, "0");
-      const seconds = String(utcNow.getUTCSeconds()).padStart(2, "0");
-      const offsetFormatted = "+09:00"; // UTC 시간이므로 오프셋은 +00:00
-      const formattedDateTime = `${year}-${month}-${date}T${hours}:${minutes}:${seconds}${offsetFormatted}`;
-      console.log("formattedDateTime: ", formattedDateTime);
-      result = await GetPopularPost(formattedDateTime);
-      //   console.log("result: ", result);
-      //   console.log("popular");
+      // 인기설문
+      result = await GetPopularPost();
+      console.log("result: ", result);
+      console.log("popular");
       setFormData(result);
     } else {
       result = await GetSurveyData(page, offset, userToken);
